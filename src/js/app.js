5f72cb45c5797d38ce32202336f07fa80aa035e6
joseriobo <joseriobo7@gmail.com>
2024-12-30 20:07:23 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -27,20 +27,27 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  if (variables.includeCover == false) {
+    cover = "<div class='cover'></div>";
+  }
+  const avatar =
+    variables.yourCover && variables.yourCover !== ""
+      ? variables.yourCover
+      : variables.background;
+  const theBackground =
+    variables.your !== "" ? variables.yourCover : variables.background;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <img src="${avatar}" class="photo" />
+          <h1>${variables.name}, ${variables.lastName} </h1>
+          <h2>${variables.role} </h2>
+          <h3>${variables.country}, ${variables.city}  </h3>
+          <ul class=${variables.socialMediaPosition}>           
+            <li>${variables.twitter}<i class="fab fa-twitter"></i></a></li>
+            <li>${variables.github}<i class="fab fa-github"></i></a></li>
+            <li>${variables.linkedin}<i class="fab fa-linkedin"></i></a></li>
+            <li>${variables.instagram}<i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -57,18 +64,19 @@ window.onload = function() {
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    name: "name",
+    lastName: "last name",
+    role: "role",
+    country: "country",
+    city: "city"
   };
   render(window.variables); // render the card for the first time
 
@@ -86,6 +94,9 @@ window.onload = function() {
           ? false
           : this.value;
       render(Object.assign(window.variables, values)); // render again the card with new values
+    });
+  });
+};es)); // render again the card with new values
     });
   });
 };
